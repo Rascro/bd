4448dd38ce72e9377900e66ba298419dc213b00a
Rascro <99190790+Rascro@users.noreply.github.com>
2022-07-15 15:16:26 -0300

    Add files via upload

---
+++ 2info3 Racro/produtos.sql
@@ -0,0 +1,30 @@
+/*insert into clientes(nome, email, login, senha, endereco_cobranca) values
+("Pedro Camargo","pedro.ribeiro@gmail.com","pedro.ribeiro","12@#$0s,k45","Rua dos Empresarios 90823"),
+("Julia Ribeiro","julia.ribeiro@gmail.com","julia.ribeiro","12@#$d83j56","Rua dos Bla78364 23"),
+("Rose Nascimento","rose@gmail.com","rose.nascimento","12@#$0osl9","Rua dos blablabla 89723"),
+("Daniela da Silva","daniela.silva@gmail.com","dani.silva","12@#$9skw4","Rua dos Comerciarios 90");*/
+
+/*insert into produtos(qtd_embalagem,preco_embalagem,qtd_estoque,desc_produto,preco_unitario)  
+values
+(1,780.32,NULL,"Notebook Lenovo",7580.36),
+(1,785.41,NULL,"Headset RGB para celular",7585.39),
+(1,781.98,NULL,"Action figure zero two",7581.32),
+(1,780.54,NULL,"NEEKO NEEKO NII",7589.34); */
+
+-- insert into comentario(cod_cliente,cod_produto,texto,titulo) values
+-- (2,3,"MINHA WAIFU <3","comentario");
+
+-- insert into fisica(cpf,rg,cod_cliente) values
+-- (80557617014,500940411,1),
+-- (97986090006,360531118,2),
+-- (02997734002,399527795,4);
+
+-- insert into itens_pedido(cod_produto,cod_pedido,qtd_prod) values
+-- (2,3,999),
+-- (1,)
+
+-- insert into pedidos(data_pedido,data_entrega,endereco_entrega,cod_cliente) values
+-- ('2022-11-01','2022-12-01',"Rua dos Comerciarios 90",51),
+-- ('2022-08-07','2022-07-01',"Rua dos Comerciarios 90",50);
+
+insert into 
